83b3d605641f16b309b24bf7fe8c4a8589b44e1c
Vaibhav Patel <patelvaibhav122003@gmail.com>
2026-01-21 16:22:44 +0530

    fix: corrected the get todos api

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -67,3 +67,86 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "Server error" }, { status: 500 });
   }
 }
+
+
+
+
+
+// Get all todos for the authenticated user
+export async function GET(req: Request) {
+  try {
+    const supabase = await supabaseServer();
+
+    // Get logged in user
+    const { data: authData, error: authError } = await supabase.auth.getUser();
+    if (authError || !authData.user)
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+    // Get user profile
+    const { data: user, error: userError } = await supabase
+      .from("profiles")
+      .select("id, role")
+      .eq("id", authData.user.id)
+      .single();
+
+    if (userError || !user)
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+    const { searchParams } = new URL(req.url);
+    const search = searchParams.get("search");
+    const status = searchParams.get("status");
+    const priority = searchParams.get("priority");
+    const userIdFilter = searchParams.get("userId");
+
+    // pagination
+    const page = Number(searchParams.get("page") || 1);
+    const limit = Number(searchParams.get("limit") || 10);
+
+    const from = (page - 1) * limit;
+    const to = from + limit - 1;
+
+
+
+
+    // filter
+    let query = supabase
+    .from("todos")
+    .select("*")
+    .order("updated_at", { ascending: false })
+    .range(from, to);
+
+    if (user.role === "user") {
+      query = query.eq("user_id", user.id);
+    }
+
+    if (userIdFilter && user.role !== "user") {
+      query = query.eq("user_id", userIdFilter);
+    }
+
+    if (search) {
+      query = query.ilike("name", `%${search}%`);
+    }
+
+    if (status) {
+      query = query.eq("status", status);
+    }
+
+    if (priority) {
+      query = query.eq("priority", priority);
+    }
+
+    
+    // admin and superadmin can see all todos
+    const { data: todos, count , error: todosError } = await query;
+
+    if (todosError)
+      return NextResponse.json(
+        { message: todosError.message },
+        { status: 500 },
+      );
+
+    return NextResponse.json({ todos, page, limit, total: count }, { status: 200 });
+  } catch {
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
+  }
+}
